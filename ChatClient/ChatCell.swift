5af9b9ccf9d1f3d8db23d46db7fade133ac4c730
navsaini <nsaini@utexas.edu>
2016-02-23 01:17:15 -0600

    First commit

---
+++ ChatClient/ChatCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatCell.swift
+//  ChatClient
+//
+//  Created by Nav Saini on 2/18/16.
+//  Copyright © 2016 Saini. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
